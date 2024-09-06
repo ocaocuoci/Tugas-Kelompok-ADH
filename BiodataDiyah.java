4d98e0cc52242911a17868205b711d445fc65a89
dyhptri <hernandar0606@gmail.com>
2024-09-06 14:17:02 +0700

    BiodataDiyah

---
+++ BiodataDiyah.java
@@ -0,0 +1,14 @@
+public class BiodataDiyah {
+    public static void main(String[] args) {
+        System.out.println("Nama         : Diyah Ramadhani Putri");
+        System.out.println("TTL          : Bandung, 26 september 2006");
+        System.out.println("Alamat       : Dusun Lebaksari, Desa Dayurejo, Jawa Timur");
+        System.out.println("Jenis Kelamin: Perempuan");
+        System.out.println("Agama        : Islam");
+        System.out.println("Email        : diyahrp10@gmail.com");
+        System.out.println("Kuliah       : Politeknik Negeri Malang");
+        System.out.println("Program Studi : Sistem Informasi Bisnis");
+        System.out.println("Hobi         : Membaca, mendengarkan music, menonton fim");
+        System.out.println("BB/TB        : 39/155");
+    }
+}
