c28c9a63d8efd1f9b1b0a5122e34a8146f178b2a
amandatodaze <amandatodaze@gmail.com>
2024-09-06 14:48:53 +0700

    biodata abel

---
+++ BiodataAbel.java
@@ -0,0 +1,14 @@
+public class BiodataAbel {
+    public static void main(String[] args) {
+        System.out.println("Nama          : Amanda Dealova Salsabiella");
+        System.out.println("TTL           : Mojokerto, 4 Februari 2006");
+        System.out.println("Alamat        : Jl. Gladiol No. 05");
+        System.out.println("Jenis Kelamin : Perempuan");
+        System.out.println("Agama         : Islam");
+        System.out.println("Email         : amandatodaze@gmail.com");
+        System.out.println("Kuliah        : Politeknik Negeri Malang");
+        System.out.println("Program Studi : Sistem Informasi Bisnis");
+        System.out.println("Hobi          : Mendengarkan Musik");
+        System.out.println("BB/TB         : 50/160");
+    }
+}
